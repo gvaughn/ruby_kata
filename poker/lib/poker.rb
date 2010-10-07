c0ccd9b0ddb31bc129caffb629a3ddab4f31557d
Greg Vaughn <gvaughn@gmail.com>
2010-10-07 10:06:13 -0500

    failing test for suit

--- poker/lib/poker.rb
+++ poker/lib/poker.rb
@@ -6,10 +6,13 @@ class Card
   {
     'A' => 14,
     'K' => 13,
+    'Q' => 12,
+    'J' => 11,
+    'T' => 10,
   }
 
   def initialize(val)
-    @face_value = val
+    @face_value = val.to_i
     @face_value = MAP[val] if MAP.has_key? val
   end
   
--- poker/spec/poker_spec.rb
+++ poker/spec/poker_spec.rb
@@ -1,27 +1,33 @@
 require File.dirname(__FILE__) + '/../lib/poker.rb'
 
 describe 'Poker' do
-  [
-    ['A','K'],
-    ['K','Q'],
-    ['Q','J'],
-    ['J','T'],
-    ['T','9'],
-    ['9','8'],
-    ['8','7'],
-    ['7','6'],
-    ['6','5'],
-    ['5','4'],
-    ['4','3'],
-    ['3','2'],
-  ].each do |cards|
-
-    first = cards[0]
-
-    second = cards[1]
-
-    it "#{first} should beat #{second}" do
-      Card.new(first).should > Card.new(second)
+  describe Card do
+    [
+      ['A','K'],
+      ['K','Q'],
+      ['Q','J'],
+      ['J','T'],
+      ['T','9'],
+      ['9','8'],
+      ['8','7'],
+      ['7','6'],
+      ['6','5'],
+      ['5','4'],
+      ['4','3'],
+      ['3','2'],
+    ].each do |first, second|
+      it "#{first} should beat #{second}" do
+        Card.new(first).should > Card.new(second)
+      end
     end
+  
+    it "equivalently accepts int or string card values" do
+      Card.new('9').should == Card.new(9)
+    end
+  
+    it "accepts suit" do
+      Card.new('KH').suit == Card.new('5H').suit
+    end
+    
   end
 end
