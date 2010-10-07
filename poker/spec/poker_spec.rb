26bd996e9e901aa411b6da16c71e12a1d9a5c731
mattflo <matt@mattflo.com>
2010-10-06 21:44:54 -0500

    Refactored spec and added some new failing ones.

--- poker/spec/poker_spec.rb
+++ poker/spec/poker_spec.rb
@@ -1,12 +1,22 @@
 require File.dirname(__FILE__) + '/../lib/poker.rb'
 
 describe 'Poker' do
-  it '9 should beat 8' do
-    Card.new(9).should > Card.new(8)
+  [
+    ['A','K'],
+    ['K','Q'],
+    ['Q','J'],
+    ['J','T'],
+    ['T','9'],
+    ['9','8'],
+    ['8','7'],
+    ['7','6'],
+    ['6','5'],
+    ['5','4'],
+    ['4','3'],
+    ['3','2'],
+  ].each do |cards|
+    it "#{cards[0]} should beat #{cards[1]}" do
+      Card.new(cards[0]).should > Card.new(cards[1])
+    end
   end
-  
-  it 'A should beat K' do
-    Card.new('A').should > Card.new('K')
-  end
-   
 end
