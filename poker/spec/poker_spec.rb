c6531b149f895c2673ec0207efedb1b1d1d255f2
Greg Vaughn <gvaughn@gmail.com>
2010-10-10 17:17:31 -0500

    here's some other specs for free. Couldn't decide on which type of next test to write: resolve paris with kickers, or full-house, or flush, or straight

--- poker/spec/poker_spec.rb
+++ poker/spec/poker_spec.rb
@@ -34,5 +34,18 @@ describe 'poker' do
     it "pair should beat nothing" do
       Hand.new('5H 5C 8S 9S 2D').should > Hand.new('2H 4C 6S 8D 10H')
     end
+    
+    it "3 of a kind should beat pair" do
+      Hand.new('4H 4C 4D 2H JC').should > Hand.new('5H 5C 8S 9S 2D')
+    end
+
+    it "4 of a kind should beat pair" do
+      Hand.new('4H 4C 4D 4S JC').should > Hand.new('5H 5C 8S 9S 2D')
+    end
+
+    it "4 of a kind should beat 3 of a kind" do
+      Hand.new('2H 2C 2D 2S JC').should > Hand.new('5H 5C 8S 9S 2D')
+    end
+
   end
 end
