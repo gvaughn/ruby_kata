ca8c636f45bc2839707dbdb380e932642315ea0f
Greg Vaughn <gvaughn@gmail.com>
2010-10-12 19:56:15 -0500

    refactor tests

--- poker/spec/poker_spec.rb
+++ poker/spec/poker_spec.rb
@@ -27,32 +27,20 @@ describe 'poker' do
   end
   
   describe 'hand' do
-    it "pair of 3s beat a pair of 2s" do
-      Hand.new('3A 3B 5C 7D 9E').should > Hand.new('2A 2B 5C 7D AE')
-    end
-
-    it "4 of a kind should beat 3 of a kind" do
-      Hand.new('2H 2C 2D 2S JC').should > Hand.new('5H 5C 8S 9S 2D')
-    end
-    
     it 'knows how many cards it consists of' do
       Hand.new('5H 5C 6S 7S KD').count == 5
     end
     
-    it "pair should beat high card" do
-      Hand.new('8H 5C 5S 9S 2D').should > Hand.new('2H 4C 6S 8D 10H')
-    end
-    
-    it "3 of a kind should beat pair" do
-      Hand.new('4H 4C 4D 2H JC').should > Hand.new('5H 5C 8S 9S 2D')
-    end
-
-    it "4 of a kind should beat pair" do
-      Hand.new('4H 4C 4D 4S JC').should > Hand.new('5H 5C 8S 9S 2D')
-    end
-
-    it "A high beats K high" do
-      Hand.new("AA 2B 4C 6D 8E").should > Hand.new("KA 2B 4C 6D 8E")
-    end
+    [["pair of 3s beat a pair of 2s",         '3A 3B 5C 7D 9E',   '2A 2B 5C 7D AE'],
+     ["4 of a kind should beat 3 of a kind",  '2H 2C 2D 2S JC',   '5H 5C 8S 9S 2D'],
+     ["pair should beat high card",           '8H 5C 5S 9S 2D',   '2H 4C 6S 8D 10H'],
+     ["3 of a kind should beat pair",         '4H 4C 4D 2H JC',   '5H 5C 8S 9S 2D'],
+     ["4 of a kind should beat pair",         '4H 4C 4D 4S JC',   '5H 5C 8S 9S 2D'],
+     ["A high beats K high",                  "AA 2B 4C 6D 8E",   "KA 2B 4C 6D 8E"]
+      ].each do |desc, winner, loser|
+        it desc do
+          Hand.new(winner).should > Hand.new(loser)
+        end
+      end
   end
 end
