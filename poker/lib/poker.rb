2d3f63e2b236e18af6efd629ac48dd7e17752ffb
Greg Vaughn <gvaughn@gmail.com>
2010-10-12 19:21:29 -0500

    tried to refactor, but it broke a test, so checking in working code with better diagnose-ability

--- poker/lib/poker.rb
+++ poker/lib/poker.rb
@@ -1,25 +1,31 @@
 class Card
   include Comparable
-  attr_reader :face_value, :suit
+  attr_reader :face_string, :face_value, :suit
   
   FACE_CARDS = {'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 11, 'T' => 10}
   FACE_MAP = Hash.new {|h,k| k.to_i}
   FACE_MAP.merge! FACE_CARDS
 
   def initialize(val)
-    @face_value = FACE_MAP[val[0,1]]
+    @face_string = val[0,1]
+    @face_value = FACE_MAP[@face_string]
     @suit = val[-1,1]
   end
   
   def <=>(other)
     face_value <=> other.face_value
   end
+  
+  def to_s
+    "#{face_string}#{suit}"
+  end
 end
 
 class Hand
   include Comparable
+  
   def initialize(val)
-    @cards = val.split.map {|c| Card.new(c)}
+    @cards = val.split.map {|c| Card.new(c)}.sort.reverse
 
     @freq_map = @cards.inject(Hash.new(0)) {|m, c| m[c.face_value] += 1; m}
   end
@@ -39,5 +45,9 @@ class Hand
   
   def strength_card
     @freq_map.max {|a,o| [a[1],a[0]] <=> [o[1],o[0]] }
+  end
+  
+  def to_s
+    @cards.join ' '
   end
 end
