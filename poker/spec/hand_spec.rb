e4c953d051fd3bed9f93edcd2bb2a480773fb706
Greg Vaughn <gvaughn@gmail.com>
2010-10-13 19:22:13 -0500

    fix spec that supplies 10 instead of T

--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -8,7 +8,7 @@ describe 'hand' do
   
   [["pair of 3s beat a pair of 2s",         '3A 3B 5C 7D 9E',   '2A 2B 5C 7D AE'],
    ["4 of a kind should beat 3 of a kind",  '2H 2C 2D 2S JC',   '5H 5C 8S 9S 2D'],
-   ["pair should beat high card",           '8H 5C 5S 9S 2D',   '2H 4C 6S 8D 10H'],
+   ["pair should beat high card",           '8H 5C 5S 9S 2D',   '2H 4C 6S 8D TH'],
    ["3 of a kind should beat pair",         '4H 4C 4D 2H JC',   '5H 5C 8S 9S 2D'],
    ["4 of a kind should beat pair",         '4H 4C 4D 4S JC',   '5H 5C 8S 9S 2D'],
    ["A high beats K high",                  "AA 2B 4C 6D 8E",   "KA 2B 4C 6D 8E"]
