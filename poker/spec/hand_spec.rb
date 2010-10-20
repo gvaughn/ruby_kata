73c41ac0cdd76f5b64066439557b62b0f6bb4c63
Greg Vaughn <gvaughn@gmail.com>
2010-10-20 13:28:52 -0500

    broken test for two small pairs vs. one big pair

--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -12,7 +12,8 @@ describe 'hand' do
    ["3 of a kind should beat pair",         '4H 4C 4D 2H JC',   '5H 5C 8S 9S 2D'],
    ["4 of a kind should beat pair",         '4H 4C 4D 4S JC',   '5H 5C 8S 9S 2D'],
    ["A high beats K high",                  "AA 2B 4C 6D 8E",   "KA 2B 4C 6D 8E"],
-   ["pairs use kicker for tie",             "8H 8S AC 2S 3D",   "8C 8D KD 9S 2C"]
+   ["pairs use kicker for tie",             "8H 8S AC 2S 3D",   "8C 8D KD 9S 2C"],
+   ["two pair 2s and 3s beats pair of As",  "2H 2S 3C 3S 4D",   "AC AD JD TS QC"],
     ].each do |desc, winner, loser|
       it desc do
         Hand.new(winner).should > Hand.new(loser)
