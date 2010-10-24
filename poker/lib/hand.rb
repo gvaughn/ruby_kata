e95dfb3da252161cfb48e039d9d2cb0b40bffb63
Greg Vaughn <gvaughn@gmail.com>
2010-10-24 13:12:08 -0500

    refactor to strength_digits method using the face values in descending frequency, instead of ascending

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -24,26 +24,29 @@ end
 class FrequencyStats
   def initialize(cards)
     @freqs = cards.reduce(Hash.new(0)) {|m, c| m[c.face_value] += 1; m}
+    def @freqs.value_by_desc_freq
+      self.sort{|a,b| b.reverse <=> a.reverse}.map{|c,f| c}
+    end
   end
   
   def max_freq
     @max_freq ||= @freqs.collect {|c, f| f}.max
   end
   
-  def sorted_progression(offset) #lousy name, getting tired
-    n = -1
-    @freqs.sort{|a,b| a.reverse <=> b.reverse}.map {|pair| n +=1; StrengthDigit.new pair.first, n + offset}
+  def strength_digits(offset)
+    max_exponent = offset + @freqs.count - 1
+    @freqs.value_by_desc_freq.map.with_index {|c,n| StrengthDigit.new c, max_exponent - n}
   end
   
   def strength
-    return sorted_progression(0)  if @freqs.count == 5  #high card
-    return sorted_progression(2)  if @freqs.count == 4 #pair
-    return sorted_progression(4)  if @freqs.count == 3 && max_freq == 2 #2 pair
-    return sorted_progression(5)  if @freqs.count == 3 && max_freq == 3 #3 of a kind
+    return strength_digits(0)  if @freqs.count == 5  #high card
+    return strength_digits(2)  if @freqs.count == 4 #pair
+    return strength_digits(4)  if @freqs.count == 3 && max_freq == 2 #2 pair
+    return strength_digits(5)  if @freqs.count == 3 && max_freq == 3 #3 of a kind
     #straight
     #flush
     #full house
-    return sorted_progression(10)  if @freqs.count == 2 && max_freq == 4 #4 of a kind
+    return strength_digits(10)  if @freqs.count == 2 && max_freq == 4 #4 of a kind
     #straight flush (royal is covered)
   end
   
@@ -71,6 +74,6 @@ class Hand
   end
 
   def to_s
-    "#{@cards.join ' '} str: #{@stats.strength.reverse.join ','}"
+    "#{@cards.join ' '} str: #{@stats.strength.join ','}"
   end
 end
