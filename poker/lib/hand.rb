eef427c8ff4ea8926b000bea29a8b7c82b132a29
Greg Vaughn <gvaughn@gmail.com>
2010-11-04 19:15:07 -0500

    introduce Characteristics class

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,33 +1,55 @@
-class Array
-  alias :face_value :first
-  alias :frequency :last
-  alias :suit :last
-end
-
 class Hand
   include Comparable
+  
+  class ::Array
+    alias :face_value :first
+    alias :frequency :last
+    alias :suit :last
+  end
+
+  class Characteristics
+    def initialize(cards)
+      @fcards = cards.reduce(Hash.new(0)) {|h, c| h[c.face_value] += 1; h}.sort{|a,b| b.reverse <=> a.reverse}
+      @fsuits = cards.reduce(Hash.new(0)) {|h, c| h[c.suit] += 1; h}
+      @gaps = cards.each_cons(2).map {|a, b| a.face_value - b.face_value}
+    end
+    
+    def num_freq
+      @fcards.count
+    end
+    
+    def max_freq
+      @fcards.first.last
+    end
+    
+    def straight?
+      @gaps.all? {|g| g == 1}
+    end
+    
+    def flush?
+      @fsuits.count == 1
+    end
+  end
   
   FACE_CARDS = {'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 11, 'T' => 10}
   FACE_MAP = Hash.new {|h,k| k.to_i}
   FACE_MAP.merge! FACE_CARDS
 
   RULES = [
-    [proc {|fcount, fmax, straight, flush| straight && flush},         9], # straight flush (and royal flush)
-    [proc {|fcount, fmax, straight, flush| fcount == 2 && fmax == 4},  8], # 4 of a kind
-    [proc {|fcount, fmax, straight, flush| fcount == 2 && fmax == 3},  7], # full house
-    [proc {|fcount, fmax, straight, flush| flush},                     6], # flush
-    [proc {|fcount, fmax, straight, flush| straight},                  5], # straight
-    [proc {|fcount, fmax, straight, flush| fcount == 3 && fmax == 3},  4], # 3 of a kind
-    [proc {|fcount, fmax, straight, flush| fcount == 3 && fmax == 2},  3], # 2 pair
-    [proc {|fcount, fmax, straight, flush| fcount == 4},               2], # pair
-    [proc {|fcount, fmax, straight, flush| fcount == 5},               1], # high card
+    [proc {|c| c.straight? && c.flush?},             9], # straight flush (and royal flush)
+    [proc {|c| c.num_freq == 2 && c.max_freq == 4},  8], # 4 of a kind
+    [proc {|c| c.num_freq == 2 && c.max_freq == 3},  7], # full house
+    [proc {|c| c.flush?},                            6], # flush
+    [proc {|c| c.straight?},                         5], # straight
+    [proc {|c| c.num_freq == 3 && c.max_freq == 3},  4], # 3 of a kind
+    [proc {|c| c.num_freq == 3 && c.max_freq == 2},  3], # 2 pair
+    [proc {|c| c.num_freq == 4},                     2], # pair
+    [proc {|c| c.num_freq == 5},                     1], # high card
   ]
   
   def initialize(str)
     @cards = str.split.map {|val| [FACE_MAP[val[0,1]], val[-1,1]]}.sort.reverse
     @fcards = @cards.reduce(Hash.new(0)) {|h, c| h[c.face_value] += 1; h}.sort{|a,b| b.reverse <=> a.reverse}
-    @fsuits = @cards.reduce(Hash.new(0)) {|h, c| h[c.suit] += 1; h}
-    @gaps = @cards.each_cons(2).map {|a, b| a.face_value - b.face_value}
   end
   
   def count
@@ -39,13 +61,7 @@ class Hand
   end
   
   def rank
-    characteristics = [
-      @fcards.count, 
-      @fcards.map(&:frequency).max, 
-      @gaps.all? {|g| g == 1},
-      @fsuits.count == 1
-    ]
-    RULES.find{|r| r.first.call(characteristics)}.last
+    RULES.find{|r| r.first.call(Characteristics.new(@cards))}.last
   end
   
   def <=>(other)
