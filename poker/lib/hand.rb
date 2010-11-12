fd2078e5e545b20c9b4fa99aaff6ca0dd9c686f9
Greg Vaughn <gvaughn@gmail.com>
2010-11-12 16:38:12 -0600

    refactor stats into Type

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -39,24 +39,44 @@ class Type
     @matcher[hand]
   end
   
+  def strength(fcards)
+    [self] << fcards.map(&:first) #it's the face_value
+  end
+  
   def <=>(other)
     rank <=> other.rank
   end
   
-  def self.for(hand)
-    RULES.find{|r| r.match(hand)}
+  def self.straight?(gaps, fcards)
+    if gaps == [9,1,1,1] # ace-low straight
+      fcards.shift #get rid of ace-high tiebreaker
+      return true
+    end
+    gaps.all? {|g| g == 1}
+  end
+  
+  def self.for(cards)
+    stats = self.stats_for(cards)
+    RULES.find{|r| r.match(stats)}.strength(stats.first)
+  end
+  
+  def self.stats_for(cards)
+    fcards = cards.reduce(Hash.new(0)) {|h, c| h[c.face_value] += 1; h}.sort{|a,b| b.reverse <=> a.reverse}
+    fsuits = cards.reduce(Hash.new(0)) {|h, c| h[c.suit] += 1; h}
+    gaps = cards.each_cons(2).map {|a, b| a.gap(b)}
+    [fcards, gaps, fsuits.count, fcards.count, fcards.first.last]
   end
 
   RULES = [
-    Type.new('straight flush', 9) {|c| c.straight? && c.flush?},
-    Type.new('quads', 8)          {|c| c.num_freq == 2 && c.max_freq == 4},
-    Type.new('full house', 7)     {|c| c.num_freq == 2 && c.max_freq == 3},
-    Type.new('flush', 6)          {|c| c.flush?},
-    Type.new('straight', 5)       {|c| c.straight?},
-    Type.new('trips', 4)          {|c| c.num_freq == 3 && c.max_freq == 3}, 
-    Type.new('2 pair', 3)         {|c| c.num_freq == 3 && c.max_freq == 2},
-    Type.new('pair', 2)           {|c| c.num_freq == 4},
-    Type.new('high card', 1)      {|c| c.num_freq == 5},
+    Type.new('straight flush', 9) {|fcards, gaps, nsuits, nfreq, mfreq| Type.straight?(gaps, fcards) && nsuits == 1},
+    Type.new('quads', 8)          {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 2 && mfreq == 4},
+    Type.new('full house', 7)     {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 2 && mfreq == 3},
+    Type.new('flush', 6)          {|fcards, gaps, nsuits, nfreq, mfreq| nsuits == 1},
+    Type.new('straight', 5)       {|fcards, gaps, nsuits, nfreq, mfreq| Type.straight?(gaps, fcards)},
+    Type.new('trips', 4)          {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 3 && mfreq == 3}, 
+    Type.new('2 pair', 3)         {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 3 && mfreq == 2},
+    Type.new('pair', 2)           {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 4},
+    Type.new('high card', 1)      {|fcards, gaps, nsuits, nfreq, mfreq| nfreq == 5},
   ]
 end
 
@@ -65,47 +85,24 @@ class Hand
   
   def initialize(str)
     @cards = str.split.map {|val| Card.new(val[0,1], val[-1,1])}.sort.reverse
-    @fcards = @cards.reduce(Hash.new(0)) {|h, c| h[c.face_value] += 1; h}.sort{|a,b| b.reverse <=> a.reverse}
-    @fsuits = @cards.reduce(Hash.new(0)) {|h, c| h[c.suit] += 1; h}
-    @gaps = @cards.each_cons(2).map {|a, b| a.gap(b)}
   end
   
   def count
     @cards.count
   end
   
+  def strength
+    @strength ||= Type.for(@cards)
+  end
+  
   def type
-    @type ||= Type.for(self)
+    strength.first
   end
   
   def <=>(other)
-    ( [type] << tie_breakers) <=> ( [other.type] << other.tie_breakers)
+    strength <=> other.strength
   end
   
-  def num_freq
-    @fcards.count
-  end
-  
-  def max_freq
-    @fcards.first.last
-  end
-  
-  def straight?
-    if @gaps == [9,1,1,1] # ace-low straight
-      @fcards.shift #get rid of ace-high tiebreaker
-      return true
-    end
-    @gaps.all? {|g| g == 1}
-  end
-  
-  def flush?
-    @fsuits.count == 1
-  end
-  
-  def tie_breakers
-    @fcards.map(&:first) #it's the face_value
-  end
-
   def to_s
     "#{type.name}: #{@cards.join ' '}"
   end
